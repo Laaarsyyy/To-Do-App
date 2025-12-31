e1c711151d934b6784bc4e04e1cb06a401f43a25
Laaarsyyy <listeriolars13@gmail.com>
2026-01-01 02:09:55 +0800

    start of color picker

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const toggleDarkMode =document.getElementById('darkModeToggle');
+const task = document.getElementsByClassName('addTaskButton');
 
 toggleDarkMode.addEventListener('change', () => {
     document.body.classList.toggle('darkMode', toggleDarkMode.checked);
