7482dd701a9acc37bee976a215a7645fe242c662
Laaarsyyy <listeriolars13@gmail.com>
2026-01-01 19:43:08 +0800

    finished custom theme feature

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const toggleDarkMode =document.getElementById('darkModeToggle');
-const task = document.getElementsByClassName('addTaskButton');
 
 toggleDarkMode.addEventListener('change', () => {
     document.body.classList.toggle('darkMode', toggleDarkMode.checked);
+});
+
+const colorPicker = document.getElementById('color');
+const colorOutput = document.getElementById('colorOutput');
+
+colorPicker.addEventListener('input', () => {
+    colorOutput.textContent = colorPicker.value;
+    document.documentElement.style.setProperty('--primary-color', colorPicker.value);
 });
