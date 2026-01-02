1ea42e40f2838eab5b9366f3ae727a3f25e447a5
Laaarsyyy <listeriolars13@gmail.com>
2026-01-02 21:34:01 +0800

    start of add task modal

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ toggleDarkMode.addEventListener('change', () => {
 });
 
 const colorPicker = document.getElementById('color');
-const colorOutput = document.getElementById('colorOutput');
 
 colorPicker.addEventListener('input', () => {
-    colorOutput.textContent = colorPicker.value;
     document.documentElement.style.setProperty('--primary-color', colorPicker.value);
 });
