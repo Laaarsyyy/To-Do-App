d0d8a59507116c5ae1dbdc03ce88e82c4d52557d
Laaarsyyy <listeriolars13@gmail.com>
2026-01-05 17:03:18 +0800

    finished add task modal

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ const colorPicker = document.getElementById('color');
 colorPicker.addEventListener('input', () => {
     document.documentElement.style.setProperty('--primary-color', colorPicker.value);
 });
+
+
+const addTaskCloseModal = document.getElementById('addTaskcloseModal');
+const addTaskModal = document.querySelector('.addTaskModal');
+
+addTaskCloseModal.addEventListener('click', () => {
+    addTaskModal.classList.remove('open');
+    addTaskModal.classList.add('closed');
+});
+
+const addTaskButton = document.querySelector('.addTaskButton');
+
+addTaskButton.addEventListener('click', () => {
+    addTaskModal.classList.remove('closed');
+    addTaskModal.classList.add('open');
+});
