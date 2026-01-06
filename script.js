96fb9ca1eebf3e82136d8f09eb999f4d53840275
Laaarsyyy <listeriolars13@gmail.com>
2026-01-06 15:53:23 +0800

    finished modal animation and appearing listfield when clicked

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+/*
+========================================
+        Toggle Dark Mode
+========================================
+*/
+
 const toggleDarkMode =document.getElementById('darkModeToggle');
 
 toggleDarkMode.addEventListener('change', () => {
     document.body.classList.toggle('darkMode', toggleDarkMode.checked);
 });
+
+/*
+========================================
+        color Picker 
+========================================
+*/
 
 const colorPicker = document.getElementById('color');
 
@@ -10,18 +22,41 @@ colorPicker.addEventListener('input', () => {
     document.documentElement.style.setProperty('--primary-color', colorPicker.value);
 });
 
+/*
+========================================
+        addList and Cancel List
+========================================
+*/
+const listSelect = document.getElementById('listSelect');
+const addListField = document.querySelector('.addListField');
+const cancelListBtn = document.getElementById('cancelListBtn');
+
+listSelect.addEventListener('change', () => {
+    if (listSelect.value === 'addList') {
+        addListField.style.display = 'flex';
+    }
+});
+
+cancelListBtn.addEventListener('click', () => {
+    addListField.style.display = 'none';
+    listSelect.value = '';
+});
+
+/*
+========================================
+        Opening and closing modal
+========================================
+*/
 
 const addTaskCloseModal = document.getElementById('addTaskcloseModal');
 const addTaskModal = document.querySelector('.addTaskModal');
-
-addTaskCloseModal.addEventListener('click', () => {
-    addTaskModal.classList.remove('open');
-    addTaskModal.classList.add('closed');
-});
-
 const addTaskButton = document.querySelector('.addTaskButton');
+const cancelTaskBtn = document.querySelector('.cancelTaskBtn');
 
-addTaskButton.addEventListener('click', () => {
-    addTaskModal.classList.remove('closed');
-    addTaskModal.classList.add('open');
-});
+const toggleModal = () => {
+    addTaskModal.classList.toggle('open');
+};
+
+addTaskCloseModal.addEventListener('click', toggleModal);
+addTaskButton.addEventListener('click', toggleModal);
+cancelTaskBtn.addEventListener('click', toggleModal);
